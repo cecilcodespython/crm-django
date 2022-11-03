3fbce718cff030182199ee46aa129e0382f054f4
cecilcodespython <107161623+cecilcodespython@users.noreply.github.com>
2022-11-03 13:13:45 +0000

    lead update and lead delete views

--- leads/urls.py
+++ leads/urls.py
@@ -5,5 +5,6 @@ urlpatterns = [
     path('',views.lead_list,name='lead_list'),
     path('create/',views.lead_create),
     path('<int:pk>/',views.lead_details,name='lead_detail'),
-    
+    path('<int:pk>/update/',views.lead_update,name='lead_update'),
+    path('<int:pk>/delete/',views.lead_delete,name='lead_update'),
 ]
--- leads/views.py
+++ leads/views.py
@@ -2,7 +2,7 @@ from venv import create
 from django.shortcuts import render
 from django.http import HttpResponse
 from .models import Lead,Agent
-from .forms import LeadForm
+from .forms import LeadForm,LeadModelForm
 from django.shortcuts import redirect
 
 # Create your views here.
@@ -21,26 +21,43 @@ def lead_details(request,pk):
 
 
 def lead_create(request):
-    form = LeadForm()
+    form = LeadModelForm()
     if request.method == "POST":
-        form = LeadForm(request.POST)
+        form = LeadModelForm(request.POST)
         if form.is_valid():
-            first_name = form.cleaned_data['first_name']
-            last_name = form.cleaned_data['last_name']
-            age = form.cleaned_data['age']
-            agent = Agent.objects.first()
-            Lead.objects.create(
-                first_name = first_name,
-                last_name = last_name,
-                age = age,
-                agent = agent,
-            )
+            form.save()
             return redirect("/leads")
 
     
     return render(request,'leads/lead_create.html',{
         'form':form
     })
+
+
+def lead_update(request,pk):
+    lead = Lead.objects.get(id=pk)
+    form = LeadModelForm(instance=lead)
+    if request.method == "POST":
+        form = LeadModelForm(request.POST,instance=lead)
+        if form.is_valid():
+            form.save()
+            return redirect("/leads")
+   
+    context = {
+            'form':form,
+            'lead':lead
+        }
+          
+            
+
+    return render(request,'leads/lead_update.html',context)
+
+
+def lead_delete(requests,pk):
+    lead = Lead.objects.get(id=pk)
+    lead.delete()
+    return redirect('/leads')
+
 
    
 
